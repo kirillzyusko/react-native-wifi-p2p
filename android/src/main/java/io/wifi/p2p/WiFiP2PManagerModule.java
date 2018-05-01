895d0beb0dd66d201b8e5ae5a8cec54c160f0150
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-05-01 17:13:33 +0300

    added initialization of p2p manager

--- android/src/main/java/io/wifi/p2p/WiFiP2PManagerModule.java
+++ android/src/main/java/io/wifi/p2p/WiFiP2PManagerModule.java
@@ -1,16 +1,24 @@
 package io.wifi.p2p;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import static android.os.Looper.getMainLooper;
+
 /**
  * Created by zyusk on 01.05.2018.
  */
 
 public class WiFiP2PManagerModule extends ReactContextBaseJavaModule {
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+
     public WiFiP2PManagerModule(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -24,5 +32,17 @@ public class WiFiP2PManagerModule extends ReactContextBaseJavaModule {
     public void log() {
         System.out.println("Hello world");
         Log.d("Hello","World");
+    }
+
+    @ReactMethod
+    public void test() {
+        Activity activity = getCurrentActivity();
+        if (activity != null) {
+            System.out.println("not null");
+            manager = (WifiP2pManager) activity.getSystemService(Context.WIFI_P2P_SERVICE);
+            System.out.println(manager); //null
+            channel = manager.initialize(activity, getMainLooper(), null);
+            System.out.println(channel);
+        }
     }
 }
