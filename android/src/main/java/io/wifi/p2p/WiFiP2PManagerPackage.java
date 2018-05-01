be7d2899ebef89a0704eb4d390cdd9428dff3fb9
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-05-01 16:49:10 +0300

    fix release bugs

--- android/src/main/java/io/wifi/p2p/WiFiP2PManagerPackage.java
+++ android/src/main/java/io/wifi/p2p/WiFiP2PManagerPackage.java
@@ -15,17 +15,14 @@ import java.util.List;
  */
 
 public class WiFiP2PManagerPackage implements ReactPackage {
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
 
