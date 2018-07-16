961a2d36366d93efadf0dee5c52c977105905cb9
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-07-16 18:51:28 +0300

    [WSB-005] - added reason code

---
+++ reason-code.js
@@ -0,0 +1,7 @@
+// STATUSES
+const ERROR = 0;
+const ERROR_MESSAGE = 'Operation failed due to an internal error';
+const P2P_UNSUPPORTED = 1;
+const P2P_UNSUPPORTED_MESSAGE = 'Operation failed because p2p is unsupported on the device';
+const BUSY = 2;
+const BUSY_ERROR = 'Operation failed because the framework is busy and unable to service the request';
