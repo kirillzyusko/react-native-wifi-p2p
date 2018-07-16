d56d4902833a811dffbd2b5a6d5392d8046db2ed
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-07-16 14:34:12 +0300

    [WSB-005] - added statuses of connection state

---
+++ device-info-statuses.js
@@ -0,0 +1,14 @@
+// DEVICE INFO STATUSES
+const CONNECTED = 0;
+const INVITED = 1;
+const FAILED = 2;
+const AVAILABLE = 3;
+const UNAVAILABLE = 4;
+
+export {
+    CONNECTED,
+    INVITED,
+    FAILED,
+    AVAILABLE,
+    UNAVAILABLE
+}
