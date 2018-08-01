8218318284e6ce8e047d764e81e6527835904773
kirikzyusko1996 <zyusko.kirik@gmail.com>
2018-08-01 16:52:04 +0300

    [WSB-007] - change package name for action

--- android/src/main/java/io/wifi/p2p/FileTransferService.java
+++ android/src/main/java/io/wifi/p2p/FileTransferService.java
@@ -21,7 +21,7 @@ import java.net.Socket;
  */
 public class FileTransferService extends IntentService {
     private static final int SOCKET_TIMEOUT = 5000;
-    public static final String ACTION_SEND_FILE = "com.example.android.wifidirect.SEND_FILE";
+    public static final String ACTION_SEND_FILE = "io.wifi.p2p.SEND_FILE";
     public static final String EXTRAS_FILE_PATH = "file_url";
     public static final String EXTRAS_GROUP_OWNER_ADDRESS = "go_host";
     public static final String EXTRAS_GROUP_OWNER_PORT = "go_port";
