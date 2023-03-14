761eb5998aa367b52ea5a5e6144f91f3a3a5ef6e
Sebinou <sebastien.legallic@gmail.com>
2023-03-14 18:05:43 +0100

    revert prettier index.js

--- index.js
+++ index.js
@@ -13,15 +13,14 @@ const MODULE_NAME = 'WIFI_P2P';
 
 const initialize = () => WiFiP2PManager.init();
 
-const startDiscoveringPeers = () =>
-  new Promise((resolve, reject) => {
+const startDiscoveringPeers = () => new Promise((resolve, reject) => {
     WiFiP2PManager.discoverPeers((reasonCode) => {
-      reasonCode === undefined ? resolve('success') : reject(getError(reasonCode));
-    });
-  });
+        reasonCode === undefined ? resolve('success') : reject(getError(reasonCode));
+    })
+});
 
 const subscribeOnEvent = (event, callback) => {
-  return DeviceEventEmitter.addListener(`${MODULE_NAME}:${event}`, callback);
+    return DeviceEventEmitter.addListener(`${MODULE_NAME}:${event}`, callback);
 };
 
 /**
@@ -29,7 +28,7 @@ const subscribeOnEvent = (event, callback) => {
  * @see https://github.com/kirillzyusko/react-native-wifi-p2p/releases/tag/3.3.0 for migration process.
  */
 const unsubscribeFromEvent = (event, callback) => {
-  DeviceEventEmitter.removeListener(`${MODULE_NAME}:${event}`, callback);
+    DeviceEventEmitter.removeListener(`${MODULE_NAME}:${event}`, callback);
 };
 
 const subscribeOnThisDeviceChanged = (callback) => subscribeOnEvent(THIS_DEVICE_CHANGED_ACTION, callback);
@@ -54,106 +53,96 @@ const subscribeOnConnectionInfoUpdates = (callback) => subscribeOnEvent(CONNECTI
  * @deprecated since RN 0.65 because of favour to new API.
  * @see https://github.com/kirillzyusko/react-native-wifi-p2p/releases/tag/3.3.0 for migration process.
  */
-const unsubscribeFromConnectionInfoUpdates = (callback) =>
-  unsubscribeFromEvent(CONNECTION_INFO_UPDATED_ACTION, callback);
+const unsubscribeFromConnectionInfoUpdates = (callback) => unsubscribeFromEvent(CONNECTION_INFO_UPDATED_ACTION, callback);
 
 const connect = (deviceAddress) => connectWithConfig({ deviceAddress });
 
-const connectWithConfig = (args) =>
-  new Promise((resolve, reject) => {
-    WiFiP2PManager.connectWithConfig(args, (status) => {
-      status === undefined ? resolve() : reject(getError(status));
-    });
-  });
+const connectWithConfig = (args) => new Promise((resolve, reject) => {
+    WiFiP2PManager.connectWithConfig(args, status => {
+        status === undefined ? resolve() : reject(getError(status));
+    })
+});
 
-const cancelConnect = () =>
-  new Promise((resolve, reject) => {
-    WiFiP2PManager.cancelConnect((status) => {
-      status === undefined ? resolve() : reject(getError(status));
-    });
-  });
+const cancelConnect = () => new Promise((resolve, reject) => {
+    WiFiP2PManager.cancelConnect(status => {
+        status === undefined ? resolve() : reject(getError(status));
+    })
+});
 
-const createGroup = () =>
-  new Promise((resolve, reject) => {
-    WiFiP2PManager.createGroup((reasonCode) => {
-      reasonCode === undefined ? resolve() : reject(getError(reasonCode));
-    });
-  });
+const createGroup = () => new Promise((resolve, reject) => {
+    WiFiP2PManager.createGroup(reasonCode => {
+        reasonCode === undefined ? resolve() : reject(getError(reasonCode));
+    })
+});
 
-const removeGroup = () =>
-  new Promise((resolve, reject) => {
-    WiFiP2PManager.removeGroup((reasonCode) => {
-      reasonCode === undefined ? resolve() : reject(getError(reasonCode));
-    });
-  });
+const removeGroup = () => new Promise((resolve, reject) => {
+    WiFiP2PManager.removeGroup(reasonCode => {
+        reasonCode === undefined ? resolve() : reject(getError(reasonCode));
+    })
+});
 
 const getAvailablePeers = () => WiFiP2PManager.getAvailablePeersList();
 
-const stopDiscoveringPeers = () =>
-  new Promise((resolve, reject) => {
-    WiFiP2PManager.stopPeerDiscovery((reasonCode) => {
-      reasonCode === undefined ? resolve() : reject(getError(reasonCode));
-    });
-  });
+const stopDiscoveringPeers = () => new Promise((resolve, reject) => {
+    WiFiP2PManager.stopPeerDiscovery(reasonCode => {
+        reasonCode === undefined ? resolve() : reject(getError(reasonCode));
+    })
+});
 
 const sendFile = (pathToFile) => WiFiP2PManager.sendFile(pathToFile);
 
 const sendFileTo = (pathToFile, address) => WiFiP2PManager.sendFileTo(pathToFile, address);
 
-const receiveFile = (folder, fileName, forceToScanGallery = false) =>
-  new Promise((resolve, reject) => {
+const receiveFile = (folder, fileName, forceToScanGallery = false) => new Promise((resolve, reject) => {
     WiFiP2PManager.receiveFile(folder, fileName, forceToScanGallery, (pathToFile) => {
-      resolve(pathToFile);
+        resolve(pathToFile);
     });
-  });
+});
 
 const sendMessage = (message) => WiFiP2PManager.sendMessage(message);
 
 const sendMessageTo = (message, address) => WiFiP2PManager.sendMessageTo(message, address);
 
-const receiveMessage = () =>
-  new Promise((resolve, reject) => {
+const receiveMessage = () => new Promise((resolve, reject) => {
     WiFiP2PManager.receiveMessage((message) => {
-      resolve(message);
+        resolve(message);
     });
-  });
+});
 
 const getConnectionInfo = () => WiFiP2PManager.getConnectionInfo();
 
 const getGroupInfo = () => WiFiP2PManager.getGroupInfo();
 
 export {
-  // public methods
-  initialize,
-  startDiscoveringPeers,
-  stopDiscoveringPeers,
-  subscribeOnThisDeviceChanged,
-  unsubscribeFromThisDeviceChanged,
-  subscribeOnPeersUpdates,
-  unsubscribeFromPeersUpdates,
-  subscribeOnConnectionInfoUpdates,
-  unsubscribeFromConnectionInfoUpdates,
-  getAvailablePeers,
-  connect,
-  connectWithConfig,
-  cancelConnect,
-  createGroup,
-  removeGroup,
-  getConnectionInfo,
-  getGroupInfo,
-  sendFile,
-  sendFileTo,
-  receiveFile,
-  sendMessage,
-  sendMessageTo,
-  receiveMessage,
+    // public methods
+    initialize,
+    startDiscoveringPeers,
+    stopDiscoveringPeers,
+    subscribeOnThisDeviceChanged,
+    unsubscribeFromThisDeviceChanged,
+    subscribeOnPeersUpdates,
+    unsubscribeFromPeersUpdates,
+    subscribeOnConnectionInfoUpdates,
+    unsubscribeFromConnectionInfoUpdates,
+    getAvailablePeers,
+    connect,
+    connectWithConfig,
+    cancelConnect,
+    createGroup,
+    removeGroup,
+    getConnectionInfo,
+    getGroupInfo,
+    sendFile,
+    receiveFile,
+    sendMessage,
+    receiveMessage,
 
-  // system methods
-  subscribeOnEvent,
-  unsubscribeFromEvent,
+    // system methods
+    subscribeOnEvent,
+    unsubscribeFromEvent,
 
-  // const
-  PEERS_UPDATED_ACTION,
-  CONNECTION_INFO_UPDATED_ACTION,
-  THIS_DEVICE_CHANGED_ACTION,
+    // const
+    PEERS_UPDATED_ACTION,
+    CONNECTION_INFO_UPDATED_ACTION,
+    THIS_DEVICE_CHANGED_ACTION,
 };
